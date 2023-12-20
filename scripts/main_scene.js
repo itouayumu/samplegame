2cb2d9999e5b3a0e772c26ca4311a7a728f96c2f
itou <a.itou.sys22@morijyobi.ac.jp>
2023-12-20 14:47:33 +0900

    当たり判定実装完了 fix #7,fix #8, fix #9

--- scripts/main_scene.js
+++ scripts/main_scene.js
@@ -1,4 +1,3 @@
-// シーンクラス
 // 他のJSファイルから呼び出された場合はシーンを返す
 class MainScene extends Phaser.Scene {
     
@@ -12,8 +11,12 @@ class MainScene extends Phaser.Scene {
     preload() {
          // 画像の読み込み(使用する時の名前, パス)
         this.load.image('back', 'assets/back.png');
+
         this.load.image('TARO', 'assets/TARO.png');
         this.load.image('HANAKO', 'assets/HANAKO.png');
+        this.load.image('APPLE', 'assets/apple.png');
+        this.load.image('orange', 'assets/orange.png');
+
 
     }
 
@@ -21,16 +24,41 @@ class MainScene extends Phaser.Scene {
     create() {
          // 単体画像をシーンに追加(X座標,Y座標,画像名)
         this.add.image(400, 300, 'back');
+
         const TARO = this.physics.add.sprite(50, 50, 'TARO');
         const HANAKO = this.physics.add.sprite(750, 400, 'HANAKO');
 
         this.TARO = TARO
 
         this.HANAKO = HANAKO
+        let fulte = this.physics.add.staticGroup();
+        for (let index = 0; index < 5; index++) {
+            let  randx1 = Phaser.Math.Between(25, 775) ; 
+            let randy1 =  Phaser.Math.Between(25, 425) ; 
+            fulte.create(randx1, randy1 , 'APPLE'); 
+            let  randx2 = Phaser.Math.Between(25, 775) ; 
+            let randy2 =  Phaser.Math.Between(25, 425) ; 
+            fulte.create(randx2, randy2 , 'orange'); 
+        }
+        this.physics.add.overlap(TARO, fulte, collectCoin, null, this);
+        function collectCoin(p,coin){
+            this.physics.pause();
+            
+        }
+        this.physics.add.overlap(HANAKO, fulte, collectCoin, null, this);
+        function collectCoin(p,coin){
+            this.physics.pause();
+           
+            
+        }
+
+            
+
         }
 
      // 毎フレーム実行される繰り返し処理
     update(time, delta) {
+
         let cursors = this.input.keyboard.createCursorKeys();
 
 
@@ -81,6 +109,3 @@ class MainScene extends Phaser.Scene {
 
     }
     }
-
-
-
